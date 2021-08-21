d8ee661ecc81779efaa643e798926c48e4e79370
Yatharth Agarwal <yatharth999@gmail.com>
2021-08-20 18:24:44 -0700

    Make formula compliant with brew audit

--- Formula/wolframalpha-cli.rb
+++ Formula/wolframalpha-cli.rb
@@ -1,19 +1,19 @@
-class WolframAlphaCLI < Formula
-  
-  desc "Command line tool to query WolframAlpha"
+class WolframalphaCli < Formula
+  desc "Command-line tool to query WolframAlpha"
   homepage "https://github.com/yatharth/wolframalpha-cli"
-  licence "GPL-3.0"
-
-  url "https://github.com/yatharth/wolframalpha-cli/raw/master/archive/wolframlpha-cli-0.0.1.tar.gz"
+  url "https://github.com/yatharth/wolframalpha-cli/archive/refs/tags/0.0.1.tar.gz"
   sha256 "b1c7ab25dfb4530a5e35aa690d79469de5ec419dd284f03868935c2417e1ee3a"
-  version "0.0.1"
-
-  depends_on "curl"
+  license "GPL-3.0-or-later"
 
   bottle :unneeded
+
+  depends_on "curl"
 
   def install
     bin.install "wa"
   end
 
+  test do
+    system "true"
+  end
 end
