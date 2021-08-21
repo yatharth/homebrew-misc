827a1472784c5bbcacb53f1e4b12053d7474bd14
Yatharth Agarwal <yatharth999@gmail.com>
2021-08-20 18:27:01 -0700

    Fix SHA hash

--- Formula/wolframalpha-cli.rb
+++ Formula/wolframalpha-cli.rb
@@ -2,7 +2,7 @@ class WolframalphaCli < Formula
   desc "Command-line tool to query WolframAlpha"
   homepage "https://github.com/yatharth/wolframalpha-cli"
   url "https://github.com/yatharth/wolframalpha-cli/archive/refs/tags/0.0.1.tar.gz"
-  sha256 "b1c7ab25dfb4530a5e35aa690d79469de5ec419dd284f03868935c2417e1ee3a"
+  sha256 "0cfe9fe674f1735f3443955dadeb4a838b928e62b305b2fad62ca10b0bdf4085"
   license "GPL-3.0-or-later"
 
   bottle :unneeded
